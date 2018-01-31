7d7e7be8cf2a0d4d9aa9089f0917bbf87880177d
Pavel Gonzales <develop.gonzales@gmail.com>
2018-01-31 16:14:08 +0300

    вынес дефайндеменшн в переменную

--- src/lib/src/component/sticky.component.ts
+++ src/lib/src/component/sticky.component.ts
@@ -21,6 +21,7 @@ export class StickyComponent implements OnInit, AfterViewInit {
     @Output() deactivated = new EventEmitter();
     @Output() reset = new EventEmitter();
 
+    public setDefineDimensions = this.defineDimensions;
     private isStuck: boolean = false;
 
     private elem: any;
